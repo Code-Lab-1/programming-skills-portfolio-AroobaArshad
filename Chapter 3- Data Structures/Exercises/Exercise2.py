9ea07172cfdfac6927f86cdb5ed2692ad33f6b89
Arooba Arshad <aroobaarshad04@gmail.com>
2022-10-25 01:19:58 +0400

    Create Exercise2.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise2.py
@@ -0,0 +1,12 @@
+Names = ["Salwa", "Rawia", "Fatima", "Seemal", "Zumaria"]
+
+msg = "Hello " + Names[0] + "!"
+print (msg)
+msg = "Hello " + Names[1] + "!"
+print (msg)
+msg = "Hello " + Names[2] + "!"
+print (msg)
+msg = "Hello " + Names[3] + "!"
+print (msg)
+msg = "Hello " + Names[4] + "!"
+print (msg)
