eb117c0c9eee218ed80a0dac55c5c0387d91ee89
Arooba Arshad <aroobaarshad04@gmail.com>
2022-10-25 02:59:49 +0400

    Update Exercise4.py

--- Chapter 3- Data Structures/Exercises/Exercise4.py
+++ Chapter 3- Data Structures/Exercises/Exercise4.py
@@ -1,10 +1,6 @@
 Guests = ["Muhammad Ali", "Bill Gates", "Margaret Thatcher", "Martin Luther King Jr."]
 
-msg = Guests[0] + ", I invite you to dinner."
-print (msg)
-msg = Guests[1] + ", I invite you to dinner."
-print (msg)
-msg = Guests[2] + ", I invite you to dinner."
-print (msg)
-msg = Guests[3] + ", I invite you to dinner."
-print (msg)
+print (Guests[0] + ", I invite you to dinner.")
+print (Guests[1] + ", I invite you to dinner.")
+print (Guests[2] + ", I invite you to dinner.")
+print (Guests[3] + ", I invite you to dinner.")
