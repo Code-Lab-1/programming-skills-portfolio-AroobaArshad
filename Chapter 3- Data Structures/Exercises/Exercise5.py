fc87a00dc5ec08e5efb71ef2e0e9b78569bd2102
Arooba Arshad <aroobaarshad04@gmail.com>
2022-10-25 03:00:44 +0400

    Update Exercise5.py

--- Chapter 3- Data Structures/Exercises/Exercise5.py
+++ Chapter 3- Data Structures/Exercises/Exercise5.py
@@ -1,13 +1,9 @@
 Guests = ["Muhammad Ali", "Bill Gates", "Margaret Thatcher", "Martin Luther King Jr."]
 
-msg = Guests[0] + ", I invite you to dinner."
-print (msg)
-msg = Guests[1] + ", I invite you to dinner."
-print (msg)
-msg = Guests[2] + ", I invite you to dinner."
-print (msg)
-msg = Guests[3] + ", I invite you to dinner."
-print (msg)
+print (Guests[0] + ", I invite you to dinner.")
+print (Guests[1] + ", I invite you to dinner.")
+print (Guests[2] + ", I invite you to dinner.")
+print (Guests[3] + ", I invite you to dinner.")
 
 print ('\n' + Guests[3] + " can't make it to dinner!")
 
@@ -16,11 +12,7 @@ del(Guests[3])
 Guests.insert(3, "Nelson Mandela")
 
 #Sending invitations again
-msg = (Guests[0]) + ", I invite you to dinner."
-print ('\n' + msg)
-msg = Guests[1] + ", I invite you to dinner."
-print (msg)
-msg = Guests[2] + ", I invite you to dinner."
-print (msg)
-msg = Guests[3] + ", I invite you to dinner."
-print (msg)
+print ('\n' + Guests[0] + ", I invite you to dinner.")
+print (Guests[1] + ", I invite you to dinner.")
+print (Guests[2] + ", I invite you to dinner.")
+print (Guests[3] + ", I invite you to dinner.")
