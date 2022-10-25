a7baf67c9c031cd91f5653199ad9e26a2954a2ad
Arooba Arshad <aroobaarshad04@gmail.com>
2022-10-26 00:42:23 +0400

    Update Exercise1.py

--- Chapter 4- Control Flow/Exercises/Exercise1.py
+++ Chapter 4- Control Flow/Exercises/Exercise1.py
@@ -1,8 +1,9 @@
 alien_color = "Green"
 if alien_color == "Green":
   print ("You just earned 5 points!")
+  
 
-
+  
 alien_color = "Red"
 if alien_color == "Green":
   print ("You just earned 5 points!")
