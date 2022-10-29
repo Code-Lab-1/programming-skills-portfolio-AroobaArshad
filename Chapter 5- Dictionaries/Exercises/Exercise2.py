bd68ba2c411370c3f3cba3c5972304537e6da64d
Arooba Arshad <aroobaarshad04@gmail.com>
2022-10-29 19:10:54 +0400

    Create Exercise2.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise2.py
@@ -0,0 +1,13 @@
+Glossary = {
+    "Dictionary":"Collection of key-value pairs.",
+    "String":"An array of characters.",
+    "List":"An ordered sequence of elements.",
+    "Variable":"Conatainer for storing data values.",
+    "Comment":"Used to provide description of a code that the python interpreter ignores." 
+    }
+
+print ("Dictionary: " + Glossary["Dictionary"])
+print ("\n" + "String: " + Glossary["String"])
+print ("\n" + "List: " + Glossary["List"])
+print ("\n" + "Variable: " + Glossary["Variable"])
+print ("\n" + "Comment: " + Glossary["Comment"])
