a425b5089f77d49c245641cf399a1ba8434db297
Arooba Arshad <aroobaarshad04@gmail.com>
2022-10-30 23:21:31 +0400

    Create Exercise3.py

---
+++ Chapter 7- Functions/Exercises/Exercise3.py
@@ -0,0 +1,7 @@
+def make_shirt (Size, Message):
+  print ("The size of the t-shirt is " + Size + ".")
+  print ("The message on the t-shirt will be " + Message + ".")
+
+make_shirt ('large', '\"Do something worth writing\"')
+print ("\n")
+make_shirt (Message = '\"Fruits Basket\"', Size = "medium")
