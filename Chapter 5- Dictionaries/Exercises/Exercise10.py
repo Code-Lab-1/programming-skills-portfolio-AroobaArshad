d8c7299e76e479d1973f98b799e227e52c1409ab
Arooba Arshad <aroobaarshad04@gmail.com>
2022-11-14 18:18:09 +0400

    Create Exercise10.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise10.py
@@ -0,0 +1,5 @@
+dict = {'Laptop':"Lenovo", 'Tablet':'Samsung', 'Mobile':'Apple', 'Television':'Philips'}
+print("Original dictionary elements:")
+print(dict)
+print("\nList of all keys of the dictionary:")
+print(list(dict.keys()))
