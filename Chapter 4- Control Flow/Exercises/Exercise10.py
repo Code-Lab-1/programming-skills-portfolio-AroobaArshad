db6ce70831b89ceb597738c5dca58a51f5094d3d
Arooba Arshad <aroobaarshad04@gmail.com>
2022-11-24 01:07:28 +0400

    Create Exercise10.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise10.py
@@ -0,0 +1,6 @@
+Character = input('You can enter any character: ')
+Vowels = "AEIOUaeiou"
+if Character in Vowels:
+    print ("The character you entered is a vowel.")
+else:
+    print ("The character you entered is not a vowel.")
